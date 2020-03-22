0bf2022264d49b57b8dea018c8c8b9541b7e695b
Ethan Rutherford <ethanrutherford2@gmail.com>
2020-03-22 16:16:44 -0700

    Update bg shader a bit
    The artificial rand I copied off the internet is (go figure) prone to less than adequate results at high values.
    To combat the issue, I simply modulo the inputs into a range where results are demonstrably adequate.
    The range is plenty large enough that users won't notice the bg repeats.
    (the region is 5000x5000 in-game units in area)

--- src/logic/background-shader.js
+++ src/logic/background-shader.js
@@ -4,8 +4,25 @@ class SpaceBgShader extends BackgroundShader {
 	constructor(gl) {
 		super(gl, `
 			varying highp vec2 vWorld;
+
+			highp float bimod(float i, float by) {
+				if (i > by) {
+					return mod(i, by);
+				}
+				if (i < -by) {
+					return -mod(-i, by);
+				}
+
+				return i;
+			}
 		
 			highp float rand(vec2 co) {
+				// the rand gets a bit odd when the inputs get too big.
+				// to combat this, loop it at 50,000
+				co.x = bimod(co.x, 50000.0);
+				co.y = bimod(co.y, 50000.0);
+
+				// original code
 				highp float a = 12.9898;
 				highp float b = 78.233;
 				highp float c = 43758.5453;
